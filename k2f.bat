7c37896a197d38ed43db3f3d0dd258f384d81aba
Christian Sciberras <uuf6429@gmail.com>
2012-10-21 17:45:40 +0200

    Fixed *nix script

--- k2f.bat
+++ k2f.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
+
+REM GENERAL NOTES
+REM - "CALL git" is used because of: http://stackoverflow.com/q/5401229/314056
+REM - Delayed expansion is required for case changing function
+REM - Ansicon is required to display colored output from scripts
+
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET ARGC=0
-FOR %%x IN (%*) DO SET /A ARGC+=1
-IF "%ARGC%" EQU "0" GOTO PROMPT
+REM Set up some variables
 
-SET RETVAL=0
 SET GITURL="https://github.com/uuf6429/K2F-2.4.git"
 SET BRANCH="master"
 SET PROGNM=%~nx0
@@ -14,49 +17,74 @@ CALL :UCase COMMND COMMND
 SET PHP="php"
 IF DEFINED ProgramFiles(x86) (SET BITNESS=64) else (SET BITNESS=32)
 SET ANSICON="tests\libs\ansicon\ansicon%BITNESS%.exe"
+SET ARGC=0
+FOR %%x IN (%*) DO SET /A ARGC+=1
 
-REM "CALL git" is used because of: http://stackoverflow.com/q/5401229/314056
+REM Parse command arguments
+SHIFT
+:PARSE_NEXT_ARG
+IF NOT "%1"=="" (
+    IF "%1"=="-b" (
+        SET BRANCH=%2
+        SHIFT
+    )
+    SHIFT
+    GOTO PARSE_NEXT_ARG
+)
 
-FOR %%L IN (BUILD TEST INIT PULL PUSH) DO IF "%COMMND%" EQU "%%L" GOTO :CASE_%COMMND%
-GOTO CASE_DEFAULT
-	:CASE_BUILD
-		%ANSICON% %PHP% tools/build.php
-		ECHO.
-		GOTO END_SWITCH
-	:CASE_TEST
-		%ANSICON% %PHP% tools/test.php
-		ECHO.
-		GOTO END_SWITCH
-	:CASE_INIT
-		CALL git init
-		CALL git remote add origin %GITURL%
-		GOTO CASE_PULL
-	:CASE_PULL
-		CALL git pull -u origin %BRANCH%
-		GOTO END_SWITCH
-	:CASE_PUSH
-		CALL git add -A
-		SET /P MSG=Commit Message: 
-		CALL git commit -m "%MSG%"
-		CALL git push -u origin %BRANCH%
-		GOTO END_SWITCH
-	:CASE_DEFAULT
-		ECHO Usage: %PROGNM% COMMAND [options]
-		ECHO.
-		ECHO List of Commands:
-		ECHO.
-		ECHO build  Generates a new build from source
-		ECHO test   Runs the tests and shows results
-		ECHO init   Install any dependencies and set up Git
-		ECHO pull   Get any updates from Git VCS system
-		ECHO push   Send any changes to Git VCS system
-		ECHO.
-		ECHO List of Options:
-		ECHO /b     Change Git branch (defaults to "master")
-		ECHO.
-		SET RETVAL=1
-		GOTO END_SWITCH
+GOTO MAINPROG REM Skip function declerations
 
+REM Dynamic prompt for when script is run directly
+:_prompt
+	SET /P EXEC=K2F^>
+	CALL %PROGNM% %EXEC%
+	ECHO.
+	GOTO :_prompt
+
+REM Main decision engine
+:_decide
+	FOR %%L IN (BUILD TEST INIT PULL PUSH) DO IF "%COMMND%" EQU "%%L" GOTO :CASE_%COMMND%
+	GOTO CASE_DEFAULT
+		:CASE_BUILD
+			%ANSICON% %PHP% tools/build.php
+			ECHO.
+			GOTO END_SWITCH
+		:CASE_TEST
+			%ANSICON% %PHP% tools/test.php
+			ECHO.
+			GOTO END_SWITCH
+		:CASE_INIT
+			CALL git init
+			CALL git remote add origin %GITURL%
+			CALL git pull -u origin %BRANCH%
+			GOTO CASE_PULL
+		:CASE_PULL
+			CALL git pull -u origin %BRANCH%
+			GOTO END_SWITCH
+		:CASE_PUSH
+			CALL git add -A
+			SET /P MSG=Commit Message: 
+			CALL git commit -m "%MSG%"
+			CALL git push -u origin %BRANCH%
+			GOTO END_SWITCH
+		:CASE_DEFAULT
+			ECHO Usage: %PROGNM% COMMAND [options]
+			ECHO.
+			ECHO List of Commands:
+			ECHO.
+			ECHO build  Generates a new build from source
+			ECHO test   Runs the tests and shows results
+			ECHO init   Install any dependencies and set up Git
+			ECHO pull   Get any updates from Git VCS system
+			ECHO push   Send any changes to Git VCS system
+			ECHO.
+			ECHO List of Options:
+			ECHO /b     Change Git branch (defaults to "master")
+			ECHO.
+			GOTO END_SWITCH
+	GOTO END_SWITCH
+
+REM Changes text to [lower/upper]case
 :LCase
 :UCase
 	SET _UCase=A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
@@ -68,11 +96,12 @@ GOTO CASE_DEFAULT
 	SET %2=%_Lib_UCase_Tmp%
 	GOTO :EOF
 
-:PROMPT
-	SET /P EXEC=K2F^>
-	CALL %EXEC%
-	ECHO.
-	GOTO :PROMPT
+REM Check for dynamic (prompt) mode
+:MAINPROG
+IF "%ARGC%" EQU "0" (
+	GOTO _prompt
+) ELSE (
+	GOTO _decide
+)
 
 :END_SWITCH
-REM EXIT %RETVAL%
--- k2f.sh
+++ k2f.sh
@@ -1,55 +1,78 @@
+
+# Set up some variables
 ARGC="$#"
-RETVAL=0
 GITURL="https://github.com/uuf6429/K2F-2.4.git"
 BRANCH="master"
-SET PROGNM="$0"
-SET COMMND="$1"
-SET PHP="php"
+PROGNM="$0"
+COMMND="$1"
+PHP="php"
 
+# Parse command arguments
+PARSE_NEXT_ARG(){
+        if [ "$1" != "" ]; then
+                if [ "$1" = "-b" ]; then
+                        BRANCH=$2
+                        shift
+                fi
+                shift
+                eval "PARSE_NEXT_ARG $@"
+        fi
+}
+eval "PARSE_NEXT_ARG $@"
+
+# Dynamic prompt for when script is run directly
 _prompt(){
 	read -p "K2F>" EXEC
-	$EXEC
+	$PROGNM $EXEC
 	echo ""
 	_prompt
 }
 
-if [ "$ARGC" = "0" ]; then _prompt
+# Main decision engine
+_decide(){
+	case $COMMND in
+		build)
+			$PHP "tools/build.php"
+			echo ""
+			;;
+		test)
+			$PHP "tools/test.php"
+			echo ""
+			;;
+		init)
+			git init
+			git remote add origin $GITURL
+			git pull -u origin $BRANCH
+			;;
+		pull)
+			git pull -u origin $BRANCH
+			;;
+		push)
+			git add -A
+			read -p "Commit Message: " MSG
+			git commit -m "$MSG"
+			git push -u origin $BRANCH
+			;;
+		*)
+			echo "Usage: $PROGNM COMMAND [options]"
+			echo ""
+			echo "List of Commands:"
+			echo ""
+			echo "build  Generates a new build from source"
+			echo "test   Runs the tests and shows results"
+			echo "init   Install any dependencies and set up Git"
+			echo "pull   Get any updates from Git VCS system"
+			echo "push   Send any changes to Git VCS system"
+			echo ""
+			echo "List of Options:"
+			echo "-b     Change Git branch (defaults to \"master\")"
+			echo ""
+	esac
+}
 
-case $COMMND in
-	build)
-		$PHP tools/build.php
-		;;
-	test)
-		$PHP tools/test.php
-		ECHO.
-		;;
-	init)
-		git init
-		git remote add origin $GITURL
-	pull)
-		git pull -u origin $BRANCH
-		;;
-	push)
-		git add -A
-		read -p "Commit Message: " MSG
-		git commit -m "$MSG"
-		git push -u origin $BRANCH
-		;;
-	*)
-		echo "Usage: $PROGNM COMMAND [options]"
-		echo ""
-		echo "List of Commands:"
-		echo ""
-		echo "build  Generates a new build from source"
-		echo "test   Runs the tests and shows results"
-		echo "init   Install any dependencies and set up Git"
-		echo "pull   Get any updates from Git VCS system"
-		echo "push   Send any changes to Git VCS system"
-		echo ""
-		echo "List of Options:"
-		echo "-b     Change Git branch (defaults to \"master\")"
-		echo ""
-		RETVAL=1
-esac
-
-exit $RETVAL
+# Check for dynamic (prompt) mode
+if [ "$ARGC" = "0" ]; then
+	_prompt
+else
+	_decide
+fi
